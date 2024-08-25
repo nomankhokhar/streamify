2fd0edf408a7e4e1f89af0ac338f73922e3bda04
Noman Ali <54282116+nomankhokhar@users.noreply.github.com>
2024-08-25 19:25:35 +0500

    Design: HomePage Designed

--- streamway/src/components/Home/Home.jsx
+++ streamway/src/components/Home/Home.jsx
@@ -12,6 +12,10 @@ import {
 } from '@chakra-ui/react';
 import './Home.css';
 import vg from '../../assets/images/bg.png';
+import { CgGoogle, CgYoutube } from 'react-icons/cg';
+import { SiCoursera, SiUdemy } from 'react-icons/si';
+import { DiAws } from 'react-icons/di';
+import introVideo from '../../assets/videos/intro.mp4';
 
 const Home = () => {
   return (
@@ -29,23 +33,50 @@ const Home = () => {
             <Text children="Streamway is a platform for streaming your favorite movies and TV shows." />
             <Link href="/courses">
               <Button size={'lg'} colorScheme="green">
-                Explore Courses 10:31
+                Explore Courses
               </Button>
             </Link>
           </VStack>
-          <Image boxSize={'md'} objectFit="contain" src={vg} alt="streamway" />
+          <Image
+            className="vector-graphics"
+            boxSize={'md'}
+            objectFit="contain"
+            src={vg}
+            alt="streamway"
+          />
         </Stack>
       </div>
 
-      <Box>
+      <Box padding={8} bg="blackAlpha.800">
         <Heading
           textAlign={'center'}
           children="OUR CREATORS"
           fontFamily={'body'}
           color={'yellow.400'}
         />
-        <HStack />
+        <HStack
+          className="brandsBanner"
+          justifyContent={'space-evenly'}
+          marginTop={'5'}
+        >
+          <CgGoogle />
+          <CgYoutube />
+          <SiCoursera />
+          <SiUdemy />
+          <DiAws />
+        </HStack>
       </Box>
+
+      <div className="container2">
+        <video
+          autoPlay
+          controls
+          src={introVideo}
+          controlsList="nodownload nofullscreen noremoteplayback"
+          disablePictureInPicture
+          disableRemotePlayback
+        ></video>
+      </div>
     </section>
   );
 };
